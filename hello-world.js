2d30d52eb53d79723a3ddcfe5520251493a46292
DarkNeko <jessada.pl@gmail.com>
2020-01-20 07:20:36 +0700

    initial commit

--- hello-world.js
+++ hello-world.js
@@ -1,14 +1,34 @@
 const http = require('http');
+const {Pool,Client}  = require('pg');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 
 const port = 8080;
 
+
+
+const pool = new Pool({
+    user: 'tangmo',
+    host: 'localhost',
+    database : 'tester',
+    password : 'tangmo',
+    port:3310,
+
+})
+
 const server = http.createServer((req,res) => {
     if(req.method ==='GET'&& req.url==='/echo'){
+        var data;
+        pool.query('select * from test.users', (err,res) => {
+            //console.log(err,res.rows[0]);
+            //data = res.rows[0];
+            //pool.end();
+            
+        })
         res.statusCode = 200;
         res.setHeader('Content-Type','text/plain');
-        res.end('Hello, World1\n');
+        res.end('data : '+data+'\n');
+        
     }else{
         res.statusCode = 200;
         res.setHeader('Content-Type','text/plain');
